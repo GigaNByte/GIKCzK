677c081ecc004f5849a6e478d721f2fb53d94f65
SquanchyDell <karolsaj0gmail.com>
2022-03-30 14:32:56 +0200

    fix

--- src/main/java/CGlab/Renderer.java
+++ src/main/java/CGlab/Renderer.java
@@ -14,7 +14,6 @@ public class Renderer {
     private BufferedImage render;
     public int h = 200;
     public int w = 200;
-
     private String filename;
     private LineAlgo lineAlgo = LineAlgo.NAIVE;
 
@@ -63,39 +62,76 @@ public class Renderer {
         int dy = y1-y0;
         float derr = Math.abs(dy/(float)(dx));
         float err = 0;
-
+            
         int y = y0;
 
         for (int x=x0; x<=x1; x++) {
             render.setRGB(x, y, white);
             err += derr;
-            if (err > 0.5) {
+            if ((err > 0.5) {
                 y += (y1 > y0 ? 1 : -1);
                 err -= 1.;
             }
         } // Oktanty ktore dzialają: 0,1,7
     }
 
-    public void drawLineBresenhamInt(int x0, int y0, int x1, int y1) {
-        int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
+   private plotLineLow(x0, y0, x1, y1){
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0){
+        yi = -1;
+        dy = -dy;
+    }
+    int D = (2 * dy) - dx;
+    int y = y0;
 
-        int dx = x1-x0;
-        int dy = y1-y0;
-
-
-        int y = y0;
-
-        int derr = 2*dy-dx;
-
-        for (int x=x0; x<=x1; x++) {
-
-            if (derr >= 0) {
-                render.setRGB(x, y, white);
-                y=y+1;
-                derr=derr+2*dy-2*dx;
+    for (x=x0; x<x1;x++){ 
+        drawPoint( x, y);
+        if (D > 0){
+            y = y + yi;
+            D = D + (2 * (dy - dx));
+        else
+            D = D + 2*dy;
+        }
+   }
+    private plotLineHigh(int x0, int y0,int x1, int y1){
+    int dx = x1 - x0;
+   int  dy = y1 - y0;
+   int xi = 1;
+    if (dx < 0){
+        xi = -1;
+        dx = -dx;
+    }
+    int D = (2 * dx) - dy;
+    int x = x0;
+        
+        for (y = y0 ;y< y1;y++){
+            drawPoint( x,  y);
+            if (D > 0){
+                x = x + xi;
+                D = D + (2 * (dx - dy));
             }else{
-                render.setRGB(x, y, white);
-                derr=derr+2*dy;
+                D = D + 2*dx;
+            }
+        }
+       
+    }
+
+    public void drawLineBresenhamInt(int x0, int y0, int x1, int y1) {
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0) {
+            if (x0 > x1){
+                plotLineLow(x1, y1, x0, y0);
+            }
+            else{
+                plotLineLow(x0, y0, x1, y1);
+            }
+        }else{
+            if (y0 > y1){
+                plotLineHigh(x1, y1, x0, y0);
+            }
+            else{
+                plotLineHigh(x0, y0, x1, y1);
             }
         }
     }
