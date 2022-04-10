4eb12b21a9eef2fc86fbad5ed811e6daaa99bbde
GigaNByte <karol.saj0@gmail.com>
2022-04-11 01:24:14 +0200

    LAB3:ZAD5 Modified and optimized drawTriangle method

--- src/main/java/CGlab/Renderer.java
+++ src/main/java/CGlab/Renderer.java
@@ -5,11 +5,13 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import javax.imageio.ImageIO;
 
 public class Renderer {
 
-    public enum LineAlgo { NAIVE, DDA, BRESENHAM, BRESENHAM_INT; }
+    public enum LineAlgo {NAIVE, DDA, BRESENHAM, BRESENHAM_INT;}
 
     private BufferedImage render;
     public int h = 200;
@@ -18,9 +20,7 @@ public class Renderer {
     private LineAlgo lineAlgo = LineAlgo.NAIVE;
 
 
-
-
-    public Renderer(String filename,Integer width,Integer height,String method) {
+    public Renderer(String filename, Integer width, Integer height, String method) {
         h = height;
         w = width;
         render = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
@@ -39,32 +39,39 @@ public class Renderer {
     }
 
     public void drawTriangle(Vec2f A, Vec2f B, Vec2f C, GigaColor color) {
-        for (int i = 0; i < w; i++) {
-            for (int j = 0; j < h; j++) {
-                Vec2f P = new Vec2f((float)i, (float)j);
-                if ( (barycentric(A, B, C, P).x > 0 && barycentric(A, B, C, P).x < 1 && barycentric(A, B, C, P).y < 1 && barycentric(A, B, C, P).y > 0   && barycentric(A, B, C, P).z > 0 && barycentric(A, B, C, P).z < 1)) {
+        Float[] xs = {A.x, B.x, C.x};
+        Float[] ys = {A.y, B.y, C.y};
+        int minx = Math.round(Collections.min(Arrays.asList(xs)));
+        int maxx = Math.round(Collections.max(Arrays.asList(xs))+ 0.5f);
+        int miny = Math.round(Collections.min(Arrays.asList(ys)));
+        int maxy = Math.round(Collections.max(Arrays.asList(ys))+ 0.5f);
+
+        for (int i = minx; i <= maxx; i++) {
+            for (int j = miny; j <= maxy; j++) {
+                Vec2f P = new Vec2f((float) i, (float) j);
+                if ((barycentric(A, B, C, P).x > 0 && barycentric(A, B, C, P).x < 1 && barycentric(A, B, C, P).y < 1 && barycentric(A, B, C, P).y > 0 && barycentric(A, B, C, P).z > 0 && barycentric(A, B, C, P).z < 1)) {
                     drawPoint(i, j, color);
                 }
             }
         }
     }
 
-    private Vec3f crossProduct(Vec3f w1,Vec3f w2){
+    private Vec3f crossProduct(Vec3f w1, Vec3f w2) {
         float x = w1.y * w2.z - w1.z * w2.y;
         float y = w1.z * w2.x - w1.x * w2.z;
         float z = w1.x * w2.y - w1.y * w2.x;
         return new Vec3f(x, y, z);
     }
 
-    public void drawPoint(int x, int y,GigaColor color) {
+    public void drawPoint(int x, int y, GigaColor color) {
         render.setRGB(x, y, (255 << 24) + (color.x << 16) + (color.y << 8) + color.z);
     }
 
     public void drawLine(int x0, int y0, int x1, int y1, LineAlgo lineAlgo) {
-        if(lineAlgo == LineAlgo.NAIVE) drawLineNaive(x0, y0, x1, y1);
-        if(lineAlgo == LineAlgo.DDA) drawLineDDA(x0, y0, x1, y1);
-        if(lineAlgo == LineAlgo.BRESENHAM) drawLineBresenham(x0, y0, x1, y1);
-        if(lineAlgo == LineAlgo.BRESENHAM_INT) drawLineBresenhamInt(x0, y0, x1, y1);
+        if (lineAlgo == LineAlgo.NAIVE) drawLineNaive(x0, y0, x1, y1);
+        if (lineAlgo == LineAlgo.DDA) drawLineDDA(x0, y0, x1, y1);
+        if (lineAlgo == LineAlgo.BRESENHAM) drawLineBresenham(x0, y0, x1, y1);
+        if (lineAlgo == LineAlgo.BRESENHAM_INT) drawLineBresenhamInt(x0, y0, x1, y1);
     }
 
     public void drawLineNaive(int x0, int y0, int x1, int y1) {
@@ -72,11 +79,11 @@ public class Renderer {
         int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
         int dy = y1 - y0;
         int dx = x1 - x0;
-        int m = dy/dx;
+        int m = dy / dx;
         int y = y0;
 
         for (int i = x0; i < x1; i++) {
-            render.setRGB(i,y,white);
+            render.setRGB(i, y, white);
             y = y + m;
         }
 
@@ -89,14 +96,14 @@ public class Renderer {
     public void drawLineBresenham(int x0, int y0, int x1, int y1) {
         int white = 255 | (255 << 8) | (255 << 16) | (255 << 24);
 
-        int dx = x1-x0;
-        int dy = y1-y0;
-        float derr = Math.abs(dy/(float)(dx));
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        float derr = Math.abs(dy / (float) (dx));
         float err = 0;
-            
+
         int y = y0;
 
-        for (int x=x0; x<=x1; x++) {
+        for (int x = x0; x <= x1; x++) {
             render.setRGB(x, y, white);
             err += derr;
             if (err > 0.5) {
@@ -106,63 +113,62 @@ public class Renderer {
         } // Oktanty ktore dzialają: 0,1,7
     }
 
-   private void  plotLineLow(int x0, int y0, int x1, int y1) {
-       int dx = x1 - x0;
-       int dy = y1 - y0;
-       int yi = 1;
-       if (dy < 0) {
-           yi = -1;
-           dy = -dy;
-       }
-       int D = (2 * dy) - dx;
-       int y = y0;
+    private void plotLineLow(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int yi = 1;
+        if (dy < 0) {
+            yi = -1;
+            dy = -dy;
+        }
+        int D = (2 * dy) - dx;
+        int y = y0;
 
-       for (int x = x0; x < x1; x++) {
-           drawPoint(x, y,new GigaColor(255,255,0));
-           if (D > 0) {
-               y = y + yi;
-               D = D + (2 * (dy - dx));
-           }else{
-               D = D + 2 * dy;
-           }
-       }
-   }
-    private void plotLineHigh(int x0, int y0,int x1, int y1){
-    int dx = x1 - x0;
-   int  dy = y1 - y0;
-   int xi = 1;
-    if (dx < 0){
-        xi = -1;
-        dx = -dx;
-    }
-    int D = (2 * dx) - dy;
-    int x = x0;
-        
-        for (int y = y0 ;y< y1;y++){
-            drawPoint( x,  y,new GigaColor(0,0,0));
-            if (D > 0){
-                x = x + xi;
-                D = D + (2 * (dx - dy));
-            }else{
-                D = D + 2*dx;
+        for (int x = x0; x < x1; x++) {
+            drawPoint(x, y, new GigaColor(255, 255, 0));
+            if (D > 0) {
+                y = y + yi;
+                D = D + (2 * (dy - dx));
+            } else {
+                D = D + 2 * dy;
             }
         }
-       
+    }
+
+    private void plotLineHigh(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int xi = 1;
+        if (dx < 0) {
+            xi = -1;
+            dx = -dx;
+        }
+        int D = (2 * dx) - dy;
+        int x = x0;
+
+        for (int y = y0; y < y1; y++) {
+            drawPoint(x, y, new GigaColor(0, 0, 0));
+            if (D > 0) {
+                x = x + xi;
+                D = D + (2 * (dx - dy));
+            } else {
+                D = D + 2 * dx;
+            }
+        }
+
     }
 
     public void drawLineBresenhamInt(int x0, int y0, int x1, int y1) {
         if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) {
-            if (x0 > x1){
+            if (x0 > x1) {
                 plotLineLow(x1, y1, x0, y0);
-            }
-            else{
+            } else {
                 plotLineLow(x0, y0, x1, y1);
             }
-        }else{
-            if (y0 > y1){
+        } else {
+            if (y0 > y1) {
                 plotLineHigh(x1, y1, x0, y0);
-            }
-            else{
+            } else {
                 plotLineHigh(x0, y0, x1, y1);
             }
         }
